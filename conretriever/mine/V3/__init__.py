45c54f2c4691a58153192d3b36f8ce78ff448f3c
BeastyZ <1015602635@qq.com>
2024-05-12 15:57:05 +0800

    delete useless code

--- conretriever/mine/V3/__init__.py
+++
@@ -1,2 +0,0 @@
-#V2:加上切换成lora训练的功能
-#V3:加上对hard negative训练的支持
--- conretriever/mine/V6/__init__.py
+++
@@ -1,6 +0,0 @@
-#V2:加上切换成lora训练的功能
-#V3:加上对hard negative训练的支持
-#V4: 加上对多检索数据集的支持，每个检索数据集的query端有对应的prompt
-#V5: 把representation token改成和RepLLAMA一模一样的 （去掉representation token前面的eos和空格）
-#V6：支持在多数据集检索时所有数据来自同一个任务
-

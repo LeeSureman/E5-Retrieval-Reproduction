5239447ac58f71b244fe96e00985a356699a58e2
BeastyZ <1015602635@qq.com>
2024-06-28 15:57:04 +0800

    add hf_to_training_data.py

---
+++ hf_to_training_data.py
@@ -0,0 +1,28 @@
+from datasets import load_dataset
+import os, jsonlines
+
+save_dir = 'training_data/reproduction'
+os.makedirs(save_dir, exist_ok=True)
+
+task_names = ['fever', 'triviaqa', 'synthetic', 'quora_duplicate', 'eli5', 'hotpot_qa', 'squad', 'fiqa', 'nq', 'msmarco_passage']
+for task_name in task_names:
+    task_data = load_dataset('BeastyZ/ConRetriever', task_name, split='train')
+    saved_data = []
+    for d in task_data:
+        if task_name == 'synthetic':
+            saved_data.append({
+                'query': d['query'],
+                'positive': d['positive'],
+                'negative': d['negative'],
+                'instruction': d['instruction']
+            })
+        else:
+            saved_data.append({
+                'query': d['query'],
+                'positive': d['positive'],
+                'negative': d['negative'],
+            })
+
+    save_path = os.path.join(save_dir, f'{task_name}.jsonl')
+    with jsonlines.open(save_path, 'w') as f:
+        f.write_all(saved_data)

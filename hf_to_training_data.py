0f9e89f7b92bd78cfd114a8554ed017f3b3b7834
BeastyZ <1015602635@qq.com>
2024-07-08 12:10:49 +0800

    modify dataset id

--- hf_to_training_data.py
+++ hf_to_training_data.py
@@ -6,7 +6,7 @@ os.makedirs(save_dir, exist_ok=True)
 
 task_names = ['fever', 'triviaqa', 'synthetic', 'quora_duplicate', 'eli5', 'hotpot_qa', 'squad', 'fiqa', 'nq', 'msmarco_passage']
 for task_name in task_names:
-    task_data = load_dataset('BeastyZ/ConRetriever', task_name, split='train')
+    task_data = load_dataset('BeastyZ/E5-R', task_name, split='train')
     saved_data = []
     for d in task_data:
         if task_name == 'synthetic':
